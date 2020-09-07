bc929a40e3aa3f549d3a2225e78764ce87ffaa10
Petrik van der Velde <petrikvandervelde@gmail.com>
2020-09-07 23:05:18 +1200

    Set environment variables for the service port and the UI directory

--- build/package/server/service.provisioning.sh
+++ build/package/server/service.provisioning.sh
@@ -2,4 +2,5 @@
 
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
-exec /usr/binservice.provisioning server --config /etc/service.provisioning/config.yaml 2>&1 | logger
+echo "Starting provisioning service"
+exec /sbin/setuser provisioning /usr/bin/service.provisioning server --config $PROVISION_CONFIG_PATH 2>&1 | logger
