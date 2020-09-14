596457ac01cc05454b8301e8d1aa9719da3aab75
Petrik van der Velde <petrikvandervelde@gmail.com>
2020-09-14 20:34:19 +1200

    Putting the struturedLogEntry back into the routelogger file because that is where it belongs

--- internal/router/routelogger.go
+++ internal/router/routelogger.go
@@ -44,3 +44,24 @@ func (l *StructuredLogger) NewLogEntry(r *http.Request) middleware.LogEntry {
 
 	return entry
 }
+
+type structuredLoggerEntry struct {
+	Logger logrus.FieldLogger
+}
+
+func (l *structuredLoggerEntry) Write(status, bytes int, header http.Header, elapsed time.Duration, extra interface{}) {
+	l.Logger = l.Logger.WithFields(logrus.Fields{
+		"resp_status":       status,
+		"resp_bytes_length": bytes,
+		"resp_elapsed_ms":   float64(elapsed.Nanoseconds()) / 1000000.0,
+	})
+
+	l.Logger.Infoln("request complete")
+}
+
+func (l *structuredLoggerEntry) Panic(v interface{}, stack []byte) {
+	l.Logger = l.Logger.WithFields(logrus.Fields{
+		"stack": string(stack),
+		"panic": fmt.Sprintf("%+v", v),
+	})
+}
