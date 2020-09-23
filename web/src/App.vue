09f09e93bb76ecf32b0b481b090b8f845bc30809
Petrik van der Velde <petrikvandervelde@gmail.com>
2020-09-23 20:32:24 +1200

    The main page should show what the router wants to show.

--- web/src/App.vue
+++ web/src/App.vue
@@ -38,20 +38,18 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default Vue.extend({
   name: 'App',
 
   components: {
-    HelloWorld
   },
 
   data: () => ({
