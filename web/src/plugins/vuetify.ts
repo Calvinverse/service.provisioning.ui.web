93318551915a7318d5db405a9d7d68ba526a1dcc
Petrik van der Velde <petrikvandervelde@gmail.com>
2020-09-28 18:56:04 +1300

    Update the colors and the icons. Original icon: categories by Marie Van den Broeck from the Noun Project

--- web/src/plugins/vuetify.ts
+++ web/src/plugins/vuetify.ts
@@ -4,4 +4,31 @@ import Vuetify from 'vuetify/lib'
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      dark: {
+        primary: {
+          base: '#276099',
+          lighten1: '#3c6fa3',
+          lighten2: '#527fad',
+          lighten3: '#678fb7',
+          lighten4: '#7d9fc1',
+          lighten5: '#93afcc',
+          darken1: '#235689',
+          darken2: '#1f4c7a',
+          darken3: '#1b436b',
+          darken4: '#17395b',
+          darken5: '#13304c'
+        },
+        secondary: '#7c7b6f',
+        accent: '#2196f3',
+        error: '#f44336',
+        warning: '#ff9800',
+        info: '#673ab7',
+        success: '#4caf50',
+        background: '#23231f'
+      }
+    },
+    dark: true
+  }
 })
