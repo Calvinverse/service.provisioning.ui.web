7ddf9307de8f04f3c9f399f76ab7e7138cb4c365
Petrik van der Velde <petrikvandervelde@gmail.com>
2020-09-28 18:54:43 +1300

    For the time being allow CORS calls to everything while we test things

--- internal/router/router.go
+++ internal/router/router.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
 	"github.com/go-chi/render"
 	"github.com/sirupsen/logrus"
 )
@@ -85,6 +86,19 @@ func (rb routerBuilder) newChiRouter(logger *logrus.Logger) *chi.Mux {
 	router.Use(rb.newStructuredLogger(logger))
 
 	router.Use(render.SetContentType(render.ContentTypeJSON))
+
+	// Basic CORS
+	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
+	router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	return router
 }
