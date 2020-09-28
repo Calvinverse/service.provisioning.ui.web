c7c21a5a05357b46595739fcae2c3ed9c03575bf
Petrik van der Velde <petrikvandervelde@gmail.com>
2020-09-28 18:59:26 +1300

    Update the about page with some comments

--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -2,4 +2,13 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+
+  <!--
+    Application version
+    Commit
+
+    License
+
+    Licenses + links of referenced components
+  -->
 </template>
