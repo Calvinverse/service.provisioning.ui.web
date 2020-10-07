031d62be6b5d24da998174b6b7df25e15ff6de47
Petrik van der Velde <petrikvandervelde@gmail.com>
2020-10-07 16:18:07 +1300

    Install vuex-module-decorators

--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -1,17 +1,17 @@
 import Vue from 'vue'
-import Vuex, { StoreOptions } from 'vuex'
-import { RootState } from './profile/RootState'
+import Vuex from 'vuex'
+import { RootState } from './RootState'
+
+import { environment } from './environment/index'
 import { profile } from './profile/index'
 
 Vue.use(Vuex)
 
-const store: StoreOptions<RootState> = {
-  state: {
-    version: '1.0.0' // a simple property
-  },
+const store = new Vuex.Store({
   modules: {
+    environment,
     profile
   }
-}
+})
 
-export default new Vuex.Store<RootState>(store)
+export default store
