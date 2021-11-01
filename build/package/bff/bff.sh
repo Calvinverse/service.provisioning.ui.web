e5ee39250697bed4f03515c60f3dedeaaaf1c50c
Petrik van der Velde <petrikvandervelde@gmail.com>
2021-11-01 22:58:35 +1300

    changes in progress

--- build/package/bff/bff.sh
+++ build/package/bff/bff.sh
@@ -3,4 +3,4 @@
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
 echo "Starting provisioning service"
-exec /sbin/setuser provisioning /usr/bin/bff server --config $PROVISION_CONFIG_PATH 2>&1 | logger
+/usr/bin/bff server --config $PROVISION_CONFIG_PATH
